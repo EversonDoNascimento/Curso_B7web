6a1a3aa0b142c78fd0471176e98cc11eded5af23
EversonDoNascimento <everson25nascimento@gmail.com>
2023-11-20 17:59:40 -0300

    Add loop mustache

--- NodeJs/Mustache/src/routes/routes.ts
+++ NodeJs/Mustache/src/routes/routes.ts
@@ -3,10 +3,54 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
+  const products = [
+    {
+      name: "Notebook",
+      price: 2500.0,
+      category: "Tecnologia",
+    },
+    {
+      name: "Smartphone",
+      price: 1200.0,
+      category: "Tecnologia",
+    },
+    {
+      name: "Livro",
+      price: 35.5,
+      category: "Leitura",
+    },
+    {
+      name: "Fones de ouvido",
+      price: 150.0,
+      category: "Tecnologia",
+    },
+    {
+      name: "Câmera digital",
+      price: 800.0,
+      category: "Fotografia",
+    },
+  ];
+  const productsList: any = [
+    "Notebook",
+    "Smartphone",
+    "Livro",
+    "Fones de ouvido",
+    "Câmera digital",
+  ];
+  const user = { name: "Everson", age: 50 };
   res.render("home", {
-    name: "Everson",
-    age: 24,
+    products,
+    productsList,
+    showWelcome: user.age >= 50 ? true : false,
   });
+});
+
+router.get("/contact", (req: Request, res: Response) => {
+  res.render("contact", {});
+});
+
+router.get("/about", (req: Request, res: Response) => {
+  res.render("about", {});
 });
 
 export default router;
